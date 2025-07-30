e3b08e4a98249ab6f5eda38060e651fd1eb8ea32
lebid15 <alayatl.tr@gmail.com>
2025-07-30 14:04:50 +0300

    Disable collectstatic temporarily

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,10 +127,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -161,4 +158,12 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
     'x-api-token',  # ⬅️ اسم الهيدر المخصص للتوكن الثابت
 ]
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
